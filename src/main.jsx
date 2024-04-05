21e5f03da432be477fa2769812402f75d760a3f5
Granovskiy Uriy <silaveri777@gmail.net>
2024-04-05 18:55:53 +0300

    27

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux'; // для зв'язку store з React компонентами App
+import { Provider } from 'react-redux'; 
 import App from './components/App/App';
-import { store } from './redux/store'; // { persistor, store } передамо в пропс для підключення Redux store до App
-// Імпорт стилів нормалізації
+import { store } from './redux/store'; 
 import 'modern-normalize';
 import './index.css';
 
