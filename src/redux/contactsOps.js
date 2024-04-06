ed85a8aa4285c972bf350a1e268c14a629ad08da
Granovskiy Uriy <silaveri777@gmail.net>
2024-04-06 22:24:01 +0300

    33

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-axios.defaults.baseURL = 'https://660fa0ed356b87a55c51da0d.mockapi.io';
+axios.defaults.baseURL = 'https://660fa0ed356b87a55c51da0d.mockapi.io/';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
